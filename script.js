bfb4170188a547a19870f4c24076454b2f104aea
keji <tomitoyo.93@gmail.com>
2020-09-07 17:17:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        const hamburger = document.getElementById('hamburger');
+        const show = document.getElementById('show')
+
+        hamburger.addEventListener('click', () => {
+            show.classList.toggle('show');
+
+        });
