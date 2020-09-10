6517e4fdf3b117ba534e87a4038e65ab98820ccd
keji <tomitoyo.93@gmail.com>
2020-09-10 14:14:51 +0100

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const sidebar = document.getElementById('openBtn');
+// const side= document.getElementById('mySidebar');
+const close= document.getElementById('closeBtn');
+// const main= document.getElementById('main');
 
-        const hamburger = document.getElementById('hamburger');
-        const show = document.getElementById('show')
+sidebar.addEventListener('click', function sideBar(){
+    document.getElementById('mySidebar').style.width = '90vw';
+    document.getElementById('main').style.marginLeft = '90vw'
 
-        hamburger.addEventListener('click', () => {
-            show.classList.toggle('show');
+})
 
-        });
+close.addEventListener('click', function closeBtn(){
+    document.getElementById('mySidebar').style.width = '0';
+    // main[0].hover.marginLeft='40vw'
+})
